bba06f85fdac925383ff4dbc0030ec9c6272b3c6
baistan <almazbekovbaistan@gmail.com>
2020-12-02 19:05:20 +0600

    creating UserLoginView

--- artistapp/artist/views.py
+++ artistapp/artist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,12 +15,19 @@ class ContentView(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
 
-class ProfileRegisterView(APIView):
+class ProfileRegisterView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def post(self,request,*args,**kwargs):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(data=serializer.data,status=status.HTTP_201_CREATED)
 
+class UserLoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request':request}
+                                           )
+        if not serializer.is_valid():
+            return Response({'User not found'})
+        user = serializer.validated_data['user']
+        token,created = Token.objects.get_or_create(user=user)
+        return Response({'token':token.key})
 
