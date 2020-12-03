61f75434823c7bbdded0b44da3dade08fc99aa23
baistan <almazbekovbaistan@gmail.com>
2020-12-03 19:09:24 +0600

    added password validation

--- artistapp/artist/serializers.py
+++ artistapp/artist/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
 
+
 class ContentSerializer(serializers.ModelSerializer):
+
+
     class Meta:
         model = Content
         fields = '__all__'
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,7 +25,10 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'email','password','profile']
 
     def create(self, validated_data):
+        password = self.validated_data.get('password')
         profile_data = validated_data.pop('profile')
         user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
         Profile.objects.create(user=user, **profile_data)
         return user
