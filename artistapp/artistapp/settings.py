4bd791c4ee39130ce55fe16e350863465df4b6a1
baistan <almazbekovbaistan@gmail.com>
2020-12-07 19:47:39 +0600

    rewriting data base

--- artistapp/artistapp/settings.py
+++ artistapp/artistapp/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'artistapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_artist',
+        'USER': 'artist',
+        'PASSWORD': '03061998',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
