eecae840545b406ff37dda4cee91282007d121da
baistan <almazbekovbaistan@gmail.com>
2020-12-09 16:13:36 +0600

    created url for CommentView

--- artistapp/artist/urls.py
+++ artistapp/artist/urls.py
@@ -6,14 +6,15 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('content',ContentView,)
 router.register('register',ProfileRegisterView)
-router.register('comment',CommentView)
+
 
 urlpatterns = [
     path('',include(router.urls)),
     path('register/',include(router.urls),name='register'),
     path('login/',UserLoginView.as_view(),name='login'),
+    path('content/comment/<int:content_id>/',commentView,name='comment-view'),
     path('', include('likes.api.urls')),
-    path('',include(router.urls),name='comment')
+
 
 
 ]
