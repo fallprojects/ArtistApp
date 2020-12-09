2c7769ab33998eb73ccfba91a47d9c8abc2ccbbe
baistan <almazbekovbaistan@gmail.com>
2020-12-09 16:13:52 +0600

    created func CommentView

--- artistapp/artist/views.py
+++ artistapp/artist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from artist import services
 
-from rest_framework.decorators import action
+from rest_framework.decorators import action, api_view
 from .models import *
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics, viewsets, views, status
@@ -35,6 +35,21 @@ class UserLoginView(ObtainAuthToken):
         return Response({'token':token.key})
 
 
-class CommentView(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+
+@api_view(['GET','POST'])
+def commentView(request,content_id):
+    if request.method == 'GET':
+        comments = Comment.objects.filter(content_id=content_id)
+        serializer = CommentSerializer(comments,many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        try:
+            content = Content.objects.get(id=content_id)
+            comment = Comment(content=content)
+        except Content.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CommentSerializer(comment,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors)
